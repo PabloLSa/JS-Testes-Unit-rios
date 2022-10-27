8a527475c493d9390b188e1e3ccc694ad68b7421
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-27 20:45:39 -0300

    8. Implemente a função myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
