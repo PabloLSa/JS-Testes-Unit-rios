2f338726773ecf9381bccacce4e0f90c971cf239
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-27 20:52:47 -0300

    correcao

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
