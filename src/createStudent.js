b42750d39128d6f64a8de56338b3ced8dbf8538e
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-10-27 19:07:18 -0300

    5. Implemente a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({
+    name,
+    feedback() {
+      return 'Eita pessoa boa!';
+    }, 
+
+  });
 
 module.exports = createStudent;
